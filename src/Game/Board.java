0588461bb7aba4148607743e102e0c74ae4d7818
Shubham Khullar <shubham5khullar@gmail.com>
2020-11-16 00:21:21 +0530

    Added levels and difficulties
    Level, Score is calculated and shown at right of the screen. Recent scores are also displayed.

--- src/Game/Board.java
+++ src/Game/Board.java
@@ -4,16 +4,19 @@ import java.awt.Color;
 import javax.swing.JFrame;
 
 public class Board {
-
+	
 	public static void main(String[] args) {
-
-		JFrame obj = new JFrame();
+		
+		JFrame obj=new JFrame();
 		Game game = new Game();
-
+		
+		
+		new Splash();
+		 
 		obj.setTitle("Snake Game");
 		obj.setSize(1194, 700);
 		obj.setLocationRelativeTo(null);
-		// obj.setBounds(10, 10, 905, 700);
+//		obj.setBounds(10, 10, 905, 700);
 		obj.setBackground(Color.DARK_GRAY);
 		obj.setResizable(false);
 		obj.setVisible(true);
