37fc3acd5bd2eb5f2e3b45dc7ec90a2a1631216d
Shubham Khullar <shubham5khullar@gmail.com>
2020-11-15 18:30:12 +0530

    Created board for Snake to run

---
+++ src/Game/Board.java
@@ -0,0 +1,22 @@
+package Game;
+
+import java.awt.Color;
+import javax.swing.JFrame;
+
+public class Board {
+
+	public static void main(String[] args) {
+
+		JFrame obj = new JFrame();
+		Game game = new Game();
+
+		obj.setTitle("Snake Game");
+		obj.setSize(1194, 700);
+		obj.setLocationRelativeTo(null);
+		// obj.setBounds(10, 10, 905, 700);
+		obj.setBackground(Color.DARK_GRAY);
+		obj.setResizable(false);
+		obj.setVisible(true);
+		obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+}
